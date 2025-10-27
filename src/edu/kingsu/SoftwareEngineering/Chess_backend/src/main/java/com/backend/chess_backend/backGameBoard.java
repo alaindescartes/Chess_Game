979e541622df01df9fb6ac3394ef9a671f97c394
Tyler Beebe <tylerbeebe.stu@kingsu.ca>
2025-10-27 16:58:18 -0600

    Added starter functions and descriptions for how they will run

--- src/edu/kingsu/SoftwareEngineering/Chess_backend/src/main/java/com/backend/chess_backend/backGameBoard.java
+++ src/edu/kingsu/SoftwareEngineering/Chess_backend/src/main/java/com/backend/chess_backend/backGameBoard.java
@@ -10,6 +10,10 @@ public class backGameBoard
     {
         gameBoard = new int[8][8];
         movesRecord = "";
+        player1 = "";
+        player2 = "";
+        piecesList = "";
+        turn = 'w';
     }
 
     public String getMovesRecord()
@@ -27,9 +31,58 @@ public class backGameBoard
         movesRecord = movesRecord + newMove;
     }
 
+    public String makeMove(char whichTurn, String requestedMove)
+    {
+        /**
+         * This function will allow the backend to verify a move request by the frontend.
+         * the string data type willl be replaced later when we know what data type it will send
+         * this is for both string mentions in the function description
+         */
+        if (whichTurn != turn)
+        {
+            return "It is not this player's turn.";
+        }
+        else
+        {
+            /**
+             * Take the requested move and check if it is one of the calculated legal moves.
+             * if (requested move is legal)
+             * {
+             * update the board in this class
+             * then send the reply back to the front end allowing the move /
+             * update the board on the frontend
+             * also, update whos turn it is
+             * }
+             * else
+             * {
+             * we return a message to the front end stating the move is not allowed
+             * }
+             */
+        }
+    }
+
+    public void updateLegalMoves()
+    {
+        /**
+         * Once a move is made, the board is in a new position.
+         * So this function will either calculate the new legal moves,
+         * or will tell legal moves to update its legal moves list
+         */
+    }
+
+    public void runAI()
+    {
+        /**
+         * This function will run when a ai needs to make a move
+         * In order to not lock up the program when the ai is running, it is likely that 
+         * the ai will be given it's own thread to run on (multithreading)
+         */
+    }
+
     private int[][] gameBoard;
     private String movesRecord;
     private String player1; //replace string with the abstract player class later
     private String player2; //replace string with the abstract player class later
     private String piecesList; //replace string with pieces class later
+    private char turn;
 }
