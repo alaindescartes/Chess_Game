0ad895d5b0450f16ceea747ae3cc229eea170829
Tyler Beebe <tylerbeebe.stu@kingsu.ca>
2025-10-27 16:17:24 -0600

    Added variables which will change data types later

--- src/edu/kingsu/SoftwareEngineering/Chess_backend/src/main/java/com/backend/chess_backend/backGameBoard.java
+++ src/edu/kingsu/SoftwareEngineering/Chess_backend/src/main/java/com/backend/chess_backend/backGameBoard.java
@@ -29,4 +29,7 @@ public class backGameBoard
 
     private int[][] gameBoard;
     private String movesRecord;
+    private String player1; //replace string with the abstract player class later
+    private String player2; //replace string with the abstract player class later
+    private String piecesList; //replace string with pieces class later
 }
